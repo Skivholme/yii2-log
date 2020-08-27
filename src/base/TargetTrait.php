5e10fcb9379f3aa3250d5255dd613608ae5bd879
Skivholme <rasmus@skivholme.com>
2020-08-27 12:28:06 +0200

    Update TargetTrait.php

--- src/base/TargetTrait.php
+++ src/base/TargetTrait.php
@@ -15,7 +15,8 @@ use yii\helpers\Json;
 trait TargetTrait
 {
 	/** @var bool Whether to log a message containing the current user name and ID. */
-	public $logUser = false;
+    public $logUser = false;
+    public $context = [];
 
 	/**
      * Returns the text display of the specified level.
@@ -36,42 +37,94 @@ trait TargetTrait
         return isset($levels[$level]) ? $levels[$level] : 'unknown';
     }
 
-	/**
-	 * Formats a log message.
-	 *
-	 * @param array $message The log message to be formatted.
-	 *
-	 * @return string
-	 */
-	public function formatMessage($message)
-	{
+    /**
+     * Processes the given log messages.
+     *
+     * @param array $messages Log messages to be processed.
+     * @param bool  $final    Whether this method is called at the end of the current application
+     */
+    public function collect($messages, $final)
+    {
+        $this->messages = array_merge(
+            $this->messages,
+            $this->filterMessages($messages, $this->getLevels(), $this->categories, $this->except)
+        );
+        $count = count($this->messages);
+        if (($count > 0) && (($final == true) || ($this->exportInterval > 0) && ($count >= $this->exportInterval))) {
+            $this->export();
+            $this->messages = [];
+        }
+    }
+
+    /**
+     * Formats a log message.
+     *
+     * @param array $message The log message to be formatted.
+     *
+     * @return string
+     */
+    public function formatMessage($message)
+    {
+        return json_encode($this->prepareMessage($message));
+    }
+
+     /**
+     * Transform log message to assoc.
+     *
+     * @param array $message The log message.
+     *
+     * @return array
+     */
+    protected function prepareMessage($message)
+    {
 		$result = [];
-		list($text, $level, $category, $timestamp) = $message;
-		$level = static::getLevelName($level);
-		$timestamp = date('c', $timestamp);
 
-		$context = $this->getContextMessage();
-		$prefix = $this->getMessagePrefix($message);
+        list($text, $level, $category, $timestamp) = $message;
 
-		$result = ArrayHelper::merge(
-			$this->parseText($text),
-			['level' => $level, 'category' => $category, '@timestamp' => $timestamp]
-		);
+        $level = Logger::getLevelName($level);
+        $timestamp = date('c', $timestamp);
+        $context = $this->getContextMessage();
+        dd($context);
+        $result = ArrayHelper::merge(
+            $this->parseText($text),
+            $context,
+            ['level' => $level, 'category' => $category, '@timestamp' => $timestamp]
+        );
 
-		if (isset($message[4]) === true && !empty($message[4])) {
-			$result['trace'] = $message[4];
-		}
+        if (isset($message[4]) === true) {
+            $result['trace'] = $message[4];
+        }
 
 		if (isset($message[5]) === true) {
 			$result['duration'] = $message[5];
 		}
 
-		$result['info'] = $prefix;
-		$result['info']['context'] = $context;
+        return $result;
+    }
 
-		//return Json::encode($result);
-		return Json::encode($result);
-	}
+    
+    /**
+     * Generates the context information to be logged.
+     *
+     * @return array
+     */
+    protected function getContextMessage()
+    {
+        $context = $this->context;
+
+        if (($this->logUser === true) && ($user = \Yii::$app->get('user', false)) !== null) {
+            /** @var \yii\web\User $user */
+            $context['userId'] = $user->getId();
+        }
+
+        foreach ($this->logVars as $name) {
+            if (empty($GLOBALS[$name]) === false) {
+                $context[ltrim($name, '_')] = & $GLOBALS[$name];
+            }
+        }
+
+        return $context;
+    }
 
 	/**
 	 * Convert's any type of log message to array.
@@ -95,7 +148,8 @@ trait TargetTrait
 					return [
 						"@message" => $text->getMessage(),
 						"file" => $text->getFile(),
-						"line" => $text->getLine(),
+                        "line" => $text->getLine(),
+                        "unique" => str_replace(\Yii::$app->basePath, '', $text->getFile().':'.$text->getLine()),
 						"trace" => $text->getTrace(),
 						"code" => $text->getCode(),
 						"Exception" => (string) $text,
